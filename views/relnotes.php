bc1aa86f31724bc043e9c683a947534ab7af9427
Codestar <codestarlive@gmail.com>
2019-01-07 10:14:47 +0300

    updates

--- views/relnotes.php
+++ views/relnotes.php
@@ -1,13 +1,20 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly. ?>
 
+<h3>v2.0.1</h3>
+<ul>
+  <li>Added: Override files feature again</li>
+  <li>Added: Validate URL function callback ( <code>cs_validate_url</code> )</li>
+  <li>Fixed: Multiple given post formats metabox hide/show issue.</li>
+</ul>
+
 <h3>v2.0</h3>
 <ul>
-  <li>Premium version release</li>
+  <li>Premium version release.</li>
 </ul>
 
 
 <h3>v1.0</h3>
 <ul>
-  <li>Initial release</li>
+  <li>Initial release.</li>
 </ul>
 
