a16a53ff79f589ee013b32fc5ac0341c2da814c8
Codestar <codestarlive@gmail.com>
2019-01-24 20:41:20 +0300

    updated relnotes

--- views/relnotes.php
+++ views/relnotes.php
@@ -1,5 +1,19 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly. ?>
 
+<h3>v2.0.2</h3>
+<ul>
+  <li>Added: Page Templates "default" option for spesific metabox hide/show.</li>
+  <li>Added: Post Formats "default" option for spesific metabox hide/show.</li>
+  <li>Added: Only allow number inputs for Spacing, Dimensions, Border, Slider, Spinner, Typography etc.</li>
+  <li>Added: ChosenJS custom options support and improved width and css style.</li>
+  <li>Fixed: Taxonomy framework jquery triggers. It was not working again after saving. It fixed now.</li>
+  <li>Fixed: Code Editor style issue for used inside Group/Repeater.</li>
+  <li>Fixed: Sortable field sortby issue.</li>
+  <li>Improved: Typography, Color, Spinner, Date, Chosen, Slider fields for used inside Group/Repeater.</li>
+  <li>Improved: All fields javascript triggers for more performance.</li>
+  <li>Improved: Customizer Framework field dependency feature.</li>
+</ul>
+
 <h3>v2.0.1</h3>
 <ul>
   <li>Added: WP Editor AJAX support for Group Field, Repeater Field and Everywhere.</li>
