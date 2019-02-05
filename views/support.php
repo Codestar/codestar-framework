8a82502e9fda8d08bfca110aff78f1731bd3049e
Codestar <codestarlive@gmail.com>
2019-02-05 19:08:43 +0300

    spellcheck

--- views/support.php
+++ views/support.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly. ?>
 
-<p>We are provide support service for premium version users. You can join to support service for submit any question after purchasing. Free version users support is limited on github.</p>
+<p>We are provide support forum for premium version users. You can join to support forum for submit any question after purchasing. Free version users support is limited on github.</p>
 
-<p><a href="http://support.codestarthemes.com/" class="button" target="_blank" rel="nofollow"><i class="fa fa-life-ring"></i> Support Service</a> -(or)- <a href="https://github.com/Codestar/codestar-framework/issues" class="button" target="_blank" rel="nofollow"><i class="fa fa-github"></i> Github</a></p>
+<p><a href="http://support.codestarthemes.com/" class="button" target="_blank" rel="nofollow"><i class="fa fa-life-ring"></i> Support Forum</a> -(or)- <a href="https://github.com/Codestar/codestar-framework/issues" class="button" target="_blank" rel="nofollow"><i class="fa fa-github"></i> Github</a></p>
