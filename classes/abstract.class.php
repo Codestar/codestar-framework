480f70c37921a7f49357c6ccc7cb434c74a236e2
Codestar <codestarlive@gmail.com>
2019-02-28 14:28:22 +0300

    v2.0.5

---
+++ classes/abstract.class.php
@@ -0,0 +1,119 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access directly.
+/**
+ *
+ * Abstract Class
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ */
+if( ! class_exists( 'CSF_Abstract' ) ) {
+  abstract class CSF_Abstract {
+
+    public $abstract     = '';
+    public $output_css   = '';
+    public $typographies = array();
+
+    public function __construct() {
+
+      // Check for embed google web fonts
+      if( ! empty( $this->args['enqueue_webfont'] ) ) {
+        add_action( 'wp_enqueue_scripts', array( &$this, 'add_enqueue_google_fonts' ), 100 );
+      }
+
+      // Check for embed custom css styles
+      if( ! empty( $this->args['output_css'] ) ) {
+        add_action( 'wp_head', array( &$this, 'add_output_css' ), 100 );
+      }
+
+    }
+
+    public function add_enqueue_google_fonts() {
+
+      if( ! empty( $this->pre_fields ) ) {
+
+        foreach( $this->pre_fields as $field ) {
+
+          $field_id     = ( ! empty( $field['id'] ) ) ? $field['id'] : '';
+          $field_type   = ( ! empty( $field['type'] ) ) ? $field['type'] : '';
+          $field_output = ( ! empty( $field['output'] ) ) ? $field['output'] : '';
+          $field_check  = ( $field_type === 'typography' || $field_output ) ? true : false;
+
+          if( $field_type && $field_id ) {
+
+            CSF::maybe_include_field( $field_type );
+
+            $class_name = 'CSF_Field_' . $field_type;
+
+            if( class_exists( $class_name ) ) {
+
+              if( method_exists( $class_name, 'output' ) || method_exists( $class_name, 'enqueue_google_fonts' ) ) {
+
+                $field_value = '';
+
+                if( $field_check && ( $this->abstract === 'options' || $this->abstract === 'customize' ) ) {
+                  $field_value = ( isset( $this->options[$field_id] ) && $this->options[$field_id] !== '' ) ? $this->options[$field_id] : '';
+                } else if( $field_check && $this->abstract === 'metabox' ) {
+                  $field_value = $this->get_meta_value( $field );
+                }
+
+                $instance = new $class_name( $field, $field_value, $this->unique, 'wp/enqueue', $this );
+
+                // typography enqueue and embed google web fonts
+                if( $field_type === 'typography' && $this->args['enqueue_webfont'] && ! empty( $field_value['font-family'] ) ) {
+                  $instance->enqueue_google_fonts();
+                }
+
+                // output css
+                if( $field_output && $this->args['output_css'] ) {
+                  $instance->output();
+                }
+
+                unset( $instance );
+
+              }
+
+            }
+
+          }
+
+        }
+
+      }
+
+      if( ! empty( $this->typographies ) && empty( $this->args['async_webfont'] ) ) {
+
+        $query  = array( 'family' => urlencode( implode( '|', $this->typographies ) ) );
+        $api    = '//fonts.googleapis.com/css';
+        $handle = 'csf-google-web-fonts-'. $this->unique;
+        $src    = esc_url( add_query_arg( $query, $api ) );
+
+        wp_enqueue_style( $handle, $src, array(), null );
+        wp_enqueue_style( $handle );
+
+      }
+
+      if( ! empty( $this->typographies ) && ! empty( $this->args['async_webfont'] ) ) {
+
+        $api = '//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+        echo '<script type="text/javascript">';
+        echo 'WebFontConfig={google:{families:['. "'" . implode( "','", $this->typographies ) . "'" .']}};';
+        echo '!function(e){var t=e.createElement("script"),s=e.scripts[0];t.src="'. $api .'",t.async=!0,s.parentNode.insertBefore(t,s)}(document);';
+        echo '</script>';
+
+      }
+
+    }
+
+    public function add_output_css() {
+
+      $this->output_css = apply_filters( "csf_{$this->unique}_output_css", $this->output_css, $this );
+
+      if ( ! empty( $this->output_css ) ) {
+        echo '<style type="text/css">'.  $this->output_css . '</style>';
+      }
+
+    }
+
+  }
+}
---
+++ views/welcome.php
@@ -0,0 +1,130 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access directly.
+/**
+ *
+ * Setup Framework Class
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ */
+if( ! class_exists( 'CSF_Welcome' ) ) {
+  class CSF_Welcome{
+
+    private static $instance = null;
+
+    public function __construct() {
+
+      if( CSF::$premium && ( ! CSF::is_active_plugin( 'codestar-framework/codestar-framework.php' ) || apply_filters( 'csf_welcome_page', true ) === false ) ) { return; }
+
+      add_action( 'admin_menu', array( &$this, 'add_about_menu' ), 0 );
+      add_filter( 'plugin_action_links', array( &$this, 'add_plugin_action_links' ), 10, 5 );
+      add_filter( 'plugin_row_meta', array( &$this, 'add_plugin_row_meta' ), 10, 2 );
+
+      $this->set_demo_mode();
+
+    }
+
+    // instance
+    public static function instance() {
+      if ( is_null( self::$instance ) ) {
+        self::$instance = new self();
+      }
+      return self::$instance;
+    }
+
+    public function add_about_menu() {
+      add_management_page( 'Codestar Framework', 'Codestar Framework', 'manage_options', 'csf-welcome', array( &$this, 'add_page_welcome' ) );
+    }
+
+    public function add_page_welcome() {
+
+      $section = ( ! empty( $_GET['section'] ) ) ? $_GET['section'] : '';
+
+      CSF::include_plugin_file( 'views/header.php' );
+
+      // safely include pages
+      switch ( $section ) {
+
+        case 'quickstart':
+          CSF::include_plugin_file( 'views/quickstart.php' );
+        break;
+
+        case 'documentation':
+          CSF::include_plugin_file( 'views/documentation.php' );
+        break;
+
+        case 'relnotes':
+          CSF::include_plugin_file( 'views/relnotes.php' );
+        break;
+
+        case 'support':
+          CSF::include_plugin_file( 'views/support.php' );
+        break;
+
+        case 'free-vs-premium':
+          CSF::include_plugin_file( 'views/free-vs-premium.php' );
+        break;
+
+        default:
+          CSF::include_plugin_file( 'views/about.php' );
+        break;
+
+      }
+
+      CSF::include_plugin_file( 'views/footer.php' );
+
+    }
+
+    public static function add_plugin_action_links( $links, $plugin_file ) {
+
+      if( $plugin_file === 'codestar-framework/codestar-framework.php' && ! empty( $links ) ) {
+        $links['csf--welcome'] = '<a href="'. admin_url( 'tools.php?page=csf-welcome' ) .'">Settings</a>';
+        if( ! CSF::$premium ) {
+          $links['csf--upgrade'] = '<a href="http://codestarframework.com/">Upgrade</a>';
+        }
+      }
+
+      return $links;
+
+    }
+
+    public static function add_plugin_row_meta( $links, $plugin_file ) {
+
+      if( $plugin_file === 'codestar-framework/codestar-framework.php' && ! empty( $links ) ) {
+        $links['csf--docs'] = '<a href="http://codestarframework.com/documentation/" target="_blank">Documentation</a>';
+      }
+
+      return $links;
+
+    }
+
+    public function set_demo_mode() {
+
+      $demo_mode = get_option( 'csf_demo_mode', false );
+
+      if( ! empty( $_GET['csf-demo'] ) ) {
+        $demo_mode = ( $_GET['csf-demo'] === 'activate' ) ? true : false;
+        update_option( 'csf_demo_mode', $demo_mode );
+      }
+
+      if( ! empty( $demo_mode ) ) {
+        CSF::include_plugin_file( 'samples/options.samples.php' );
+
+        if( CSF::$premium ) {
+
+          CSF::include_plugin_file( 'samples/customize-options.samples.php' );
+          CSF::include_plugin_file( 'samples/metabox.samples.php'           );
+          CSF::include_plugin_file( 'samples/shortcoder.samples.php'        );
+          CSF::include_plugin_file( 'samples/taxonomy-options.samples.php'  );
+          CSF::include_plugin_file( 'samples/widgets.samples.php'           );
+
+        }
+
+      }
+
+    }
+
+  }
+
+  CSF_Welcome::instance();
+}
