8f903ce45375c83517c9b6ddc6a63e311575c49e
AitGamers <37729402+AitGamers@users.noreply.github.com>
2020-11-05 14:55:46 +0000

    Update repeater.php
    Making sanitize and validate work in repeated and tabbed fields

--- fields/repeater/repeater.php
+++ fields/repeater/repeater.php
@@ -65,6 +65,10 @@ if ( ! class_exists( 'CSF_Field_repeater' ) ) {
               $field_unique = ( ! empty( $this->unique ) ) ? $this->unique .'['. $this->field['id'] .']['. $num .']' : $this->field['id'] .'['. $num .']';
               $field_value  = ( isset( $field['id'] ) && isset( $this->value[$key][$field['id']] ) ) ? $this->value[$key][$field['id']] : '';
 
+              if (isset($args['_error_repeat']) && isset($args['_error_repeat'][$key]) && isset($args['_error_repeat'][$key][$field['id']])){
+                  $field['_error'] = $args['_error_repeat'][$key][$field['id']];
+              }
+              
               CSF::field( $field, $field_value, $field_unique, 'field/repeater' );
 
             }
